a381c9b5f9419e968b2d79ab308f30f80b823246
hemiahwu <309595700@qq.com>
2022-03-03 11:33:01 +0800

    07show images

--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="container py-3">
     <ActionsBar />
+    <div class="d-flex justify-content-between align-items-center py-2">
+      <h6 class="text-muted mb-0">文件</h6>
+      <button class="rounded-button">
+        <icon-arrow-up />
+      </button>
+    </div>
+    <div class="row">
+      <div class="col-md-3" v-for="(item, index) in 6" :key="index">
+        <div class="card mb-4">
+          <img
+            v-if="true"
+            class="file-thumb"
+            src="https://picsum.photos/id/1015/400/160"
+          />
+          <div v-else class="card-body text-center py-5">
+            <icon-type-common height="4em" width="4em" />
+          </div>
+          <div class="card-footer">
+            <div class="d-flex align-items-center">
+              <icon-type-common />
+              <span class="file-name">File 8.png</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
