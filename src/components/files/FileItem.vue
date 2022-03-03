ed5aad0f242f128f606e2387ff8457300eaa6bea
hemiahwu <309595700@qq.com>
2022-03-03 18:10:00 +0800

    11动态icon

--- src/components/files/FileItem.vue
+++ src/components/files/FileItem.vue
@@ -2,16 +2,16 @@
   <div class="col-md-3">
     <div class="card mb-4">
       <img
-        v-if="file.url"
+        v-if="isValidImage"
         class="file-thumb"
         src="https://picsum.photos/id/1015/400/160"
       />
       <div v-else class="card-body text-center py-5">
-        <icon-type-common height="4em" width="4em" />
+        <component :is="iconFileType" height="4em" width="4em" />
       </div>
       <div class="card-footer">
         <div class="d-flex align-items-center">
-          <icon-type-common />
+          <component :is="iconFileType" />
           <span class="file-name">{{ file.name }}</span>
         </div>
       </div>
@@ -20,7 +20,30 @@
 </template>
 
 <script setup>
-defineProps({
+import { computed } from "vue";
+
+const iconFileType = computed(() => {
+  const iconTypes = {
+    "image/jpeg": "icon-type-image",
+    "image/jpg": "icon-type-image",
+    "image/png": "icon-type-image",
+    "application/zip": "icon-type-zip",
+    "application/msword": "icon-type-doc",
+    "application/vnd.ms-excel": "icon-type-excel",
+    "application/pdf": "icon-type-pdf",
+    "video/mp4": "icon-type-video",
+  };
+  return iconTypes[props.file.mimeType]
+    ? iconTypes[props.file.mimeType]
+    : "icon-type-common";
+});
+
+const isValidImage = computed(() => {
+  const imageMimeTypes = ["image/jpeg", "image/jpg", "image/png"];
+  return imageMimeTypes.includes(props.file.mimeType) && !!props.file.url;
+});
+
+const props = defineProps({
   file: {
     type: Object,
     required: true,
