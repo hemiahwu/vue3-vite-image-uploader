420f2d367e16367a0736c49f2a320d859dc072cc
hemiahwu <309595700@qq.com>
2022-03-03 11:24:54 +0800

    06注册全局icon组件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import icons from './components/icons/all'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+for (const [key, value] of Object.entries(icons)) {
+    app.component(key, value)
+}
+
+app.mount('#app')
