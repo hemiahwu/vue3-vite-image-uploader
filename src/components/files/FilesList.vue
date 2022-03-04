029191e9251d392e943c283b4a3621bc0bec2649
hemiahwu <309595700@qq.com>
2022-03-04 19:06:40 +0800

    17danduoxuan

--- src/components/files/FilesList.vue
+++ src/components/files/FilesList.vue
@@ -1,11 +1,38 @@
 <template>
   <div class="row">
-    <FileItem v-for="(file, index) in files" :key="index" :file="file" />
+    <FileItem
+      v-for="(file, index) in files"
+      :key="index"
+      :file="file"
+      @click.exact="selectOne(file)"
+      @click.meta.exact="selectMultiple(file)"
+      :class="{ 'selected-file': isSelected(file) }"
+    />
   </div>
 </template>
 
 <script setup>
 import FileItem from "./FileItem.vue";
+import { reactive } from "vue";
+
+const selectedItems = reactive(new Set());
+
+const selectOne = (item) => {
+  selectedItems.clear();
+  selectedItems.add(item);
+};
+
+const selectMultiple = (item) => {
+  if (selectedItems.has(item)) {
+    selectedItems.delete(item);
+  } else {
+    selectedItems.add(item);
+    console.log(selectedItems);
+  }
+};
+
+const isSelected = (item) => selectedItems.has(item);
+
 defineProps({
   files: {
     type: Array,
